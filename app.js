e4564e765f2f67922284f0db2eb918ac684715e9
Marco Antônio Oliari <marcoaoliari@gmail.com>
2020-05-06 20:41:39 -0300

    start

--- app.js
+++ app.js
@@ -41,6 +41,8 @@ passport.deserializeUser(User.deserializeUser());
 app.use(userRoutes);
 app.use(postRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
-	console.log("Server online")
-})
+app.listen(3000)
+
+// app.listen(process.env.PORT, process.env.IP, function(){
+// 	console.log("Server online")
+// })
